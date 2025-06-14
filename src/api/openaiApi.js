1632952e7428b4b3865af6ccda57ff863de5d41f
Tushar-1745 <tusharmadane729@gmail.com>
2025-06-14 20:51:17 +0530

    openai-backend

--- src/api/openaiApi.js
+++ src/api/openaiApi.js
@@ -15,7 +15,7 @@
 //   return response.data.reply;
 // }
 
-
+import axios from "axios";
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export async function getOpenAIFeedback(question, answer) {
